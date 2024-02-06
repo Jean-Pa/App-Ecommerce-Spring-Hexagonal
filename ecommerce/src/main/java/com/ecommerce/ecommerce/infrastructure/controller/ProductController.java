d23c65426fa595f93f01c8a0116d2405b2437291
Jeancarlo Palacios <jcarlo_p_p@hotmail.com>
2024-02-06 01:59:10 -0500

    implementando el id en el template editar

--- ecommerce/src/main/java/com/ecommerce/ecommerce/infrastructure/controller/ProductController.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/infrastructure/controller/ProductController.java
@@ -3,9 +3,12 @@ package com.ecommerce.ecommerce.infrastructure.controller;
 
 import com.ecommerce.ecommerce.application.service.ProductService;
 import com.ecommerce.ecommerce.domain.Product;
+import com.ecommerce.ecommerce.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -34,8 +37,21 @@ public class ProductController {
     }
 
     @GetMapping("/show")
-    public String showProduct(){
+    public String showProduct(Model model){
+        User user = new User();
+        user.setId(1L);
+        Iterable<Product> products= productService.getProductByUser(user);
+        model.addAttribute("products",products);
 
         return "admin/products/show";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editProduct(@PathVariable Long id, Model model){
+        Product product=productService.getProductById(id);
+
+        model.addAttribute("product",product);
+        return "admin/products/edit";
+
     }
 }
