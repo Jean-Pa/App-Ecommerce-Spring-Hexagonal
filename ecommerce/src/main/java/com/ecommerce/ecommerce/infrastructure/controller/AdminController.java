152b998eb3cf3a98e3428fbf69786916fe415df5
Jeancarlo Palacios <jcarlo_p_p@hotmail.com>
2024-02-06 02:52:51 -0500

    agregando el home del administrador

--- ecommerce/src/main/java/com/ecommerce/ecommerce/infrastructure/controller/AdminController.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/infrastructure/controller/AdminController.java
@@ -1,7 +1,12 @@
 package com.ecommerce.ecommerce.infrastructure.controller;
 
 
+import com.ecommerce.ecommerce.application.service.ProductService;
+import com.ecommerce.ecommerce.domain.Product;
+import com.ecommerce.ecommerce.domain.User;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
+    private  final ProductService productService;
+
+    public AdminController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping
-    public String home(){
+    public String home(Model model){
+        User user = new User();
+        user.setId(1L);
+        Iterable<Product> products= productService.getProductByUser(user);
+        model.addAttribute("products",products);
         return "admin/home";
     }
 }
