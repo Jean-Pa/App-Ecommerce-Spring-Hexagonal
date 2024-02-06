224f6eafded65d7e497e279fd0b59f60d4fb3eca
Jeancarlo Palacios <jcarlo_p_p@hotmail.com>
2024-02-05 20:13:33 -0500

    añadiendo el postmapping en el producto

--- ecommerce/src/main/java/com/ecommerce/ecommerce/infrastructure/controller/ProductController.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/infrastructure/controller/ProductController.java
@@ -1,18 +1,27 @@
 package com.ecommerce.ecommerce.infrastructure.controller;
 
 
+import com.ecommerce.ecommerce.domain.Product;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin/products")
+@Slf4j
 public class ProductController {
 
 
     @GetMapping("/create")
     public String create(){
 
+        return "admin/products/create";
+    }
+
+    @PostMapping("/save-product")
+    public String saveProduct(Product product){
         return "admin/products/create";
     }
 }
