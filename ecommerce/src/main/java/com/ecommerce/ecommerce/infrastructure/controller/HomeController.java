7765d5d6d76916cfd1e15fd59185acd45cd9cf1a
Jean-pa <jcarlo_p_p@hotmail.com>
2024-02-17 17:44:32 -0500

    añadiendo el stock de productos para el usuario

--- ecommerce/src/main/java/com/ecommerce/ecommerce/infrastructure/controller/HomeController.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/infrastructure/controller/HomeController.java
@@ -1,14 +1,49 @@
 package com.ecommerce.ecommerce.infrastructure.controller;
 
+import com.ecommerce.ecommerce.application.service.ProductService;
+import com.ecommerce.ecommerce.application.service.StockService;
+import com.ecommerce.ecommerce.domain.Stock;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/home")
+@Slf4j
 public class HomeController {
+
+    private final ProductService productService;
+    private final StockService stockService;
+
+    public HomeController(ProductService productService, StockService stockService) {
+        this.productService = productService;
+        this.stockService = stockService;
+    }
+
+
+
     @GetMapping
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("products",productService.getProduct());
         return "home";
+    }
+
+    @GetMapping("/product-detail/{id}")
+    public String productDetail(@PathVariable Long id, Model model){
+        List<Stock> stocks = stockService.getStockByProduct(productService.getProductById(id));
+
+        //Integer lastBalance = stocks.get(stocks.size()).getBalance();
+
+        model.addAttribute("product", productService.getProductById(id));
+       // model.addAttribute("stock",lastBalance);
+
+
+
+        return "user/productdetail";
     }
 }
