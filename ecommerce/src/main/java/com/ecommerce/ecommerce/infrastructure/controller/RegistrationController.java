0e1740ef0fa33cd76bbe87696b14fe2da7906c9f
Jean-pa <jcarlo_p_p@hotmail.com>
2024-02-22 03:46:48 -0500

    añadiendo el formulario de registro de usuario

--- ecommerce/src/main/java/com/ecommerce/ecommerce/infrastructure/controller/RegistrationController.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/infrastructure/controller/RegistrationController.java
@@ -1,15 +1,39 @@
 package com.ecommerce.ecommerce.infrastructure.controller;
 
+import com.ecommerce.ecommerce.application.service.RegistrationService;
+import com.ecommerce.ecommerce.domain.User;
+import com.ecommerce.ecommerce.domain.UserType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/register")
 public class RegistrationController {
 
+    private final RegistrationService registrationService;
+
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+
     @GetMapping
     public String register(){
         return "register";
+    }
+
+
+    @PostMapping
+    public String registerUser(User user){
+        user.setDateCreated(LocalDateTime.now());
+        user.setUserType(UserType.USER);
+        user.setUsername(user.getEmail());
+        registrationService.register(user);
+        return "redirect:/register";
+
     }
 }
