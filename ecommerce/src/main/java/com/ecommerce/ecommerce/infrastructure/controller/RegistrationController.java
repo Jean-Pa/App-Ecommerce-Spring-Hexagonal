bb5ec23d4bbae1772f327a6c6100eabd33be9bad
Jean-pa <jcarlo_p_p@hotmail.com>
2024-02-22 04:24:01 -0500

    añadiendo el userDto para el formulario

--- ecommerce/src/main/java/com/ecommerce/ecommerce/infrastructure/controller/RegistrationController.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/infrastructure/controller/RegistrationController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 
@@ -27,16 +28,16 @@ public class RegistrationController {
 
 
     @GetMapping
-    public String register(){
+    public String register(UserDto userDto){
         return "register";
     }
 
 
     //Con valid hace la validacion
-    //Con BindingResult me puede decir si han ocurrido errores, osea que el usuario no
+    //Con BindingResult nos guarda los errores y me puede decir si han ocurrido errores, osea que el usuario no
     //escribio los campos que validamos
     @PostMapping
-    public String registerUser(@Valid UserDto userDto, BindingResult bindingResult){
+    public String registerUser(@Valid UserDto userDto, BindingResult bindingResult, RedirectAttributes redirectAttributes){
 //        user.setDateCreated(LocalDateTime.now());
 //        user.setUserType(UserType.USER);
 //        user.setUsername(user.getEmail());
@@ -46,11 +47,15 @@ public class RegistrationController {
             //Aqui traemos todo  y lo recorremos y vemos cual estuvo con error
             bindingResult.getAllErrors().forEach(
                     //con la variable e vemos todos los errores y en las llaves iteraria
-                    e->{log.info("Errors {}",e.getDefaultMessage());}
+                    e->{log.info("Error {}",e.getDefaultMessage());}
             );
+
+            //Retornamos al registro para que asi nos mande el error
+            return "register";
         }
         //Aqui pasamos el objeto de tipo usuario que ya lo estamos traendo del DTO
         registrationService.register(userDto.userDtoToUser());
+        redirectAttributes.addFlashAttribute("success","Usuario creado correctamente");
         return "redirect:/register";
 
     }
